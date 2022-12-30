37d8d22acb8b3c30a7546595b463c69084fadcba
Haritha_Mohan <89852956+harithamohan123@users.noreply.github.com>
2022-12-30 20:57:27 +0530

    Add files via upload

---
+++ Facial Emotion Rcognition using opencv.py
@@ -0,0 +1,17 @@
+from facial_emotion_recognition import EmotionRecognition
+import cv2 as cv
+
+er = EmotionRecognition(device='cpu')
+
+cam = cv.VideoCapture(0)
+
+while True:
+    success, frame = cam.read()
+    frame = er.recognise_emotion(frame, return_type = 'BGR')
+    cv.imshow('frame'. frame)
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
